50551e58d860f43586fd8dfd9b543c79ba23a96e
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2022-03-02 20:56:16 +0500

    Custom News Ticker Plugin

---
+++ custom-news-ticker.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * Plugin Name: Custom News Ticker
+ * Plugin URI: http://www.glowing-tips.com/
+ * Description: Showing Latest Posts With Effects
+ * Author: Hafiz Abubakar
+ * Author URI: http://www.glowing-tips.com/
+ * Version: 1.0
+ * Test Domain: custom-news-ticker
+ */
+   $my_plugin_name = "Custom News Ticker";
+   $var_prefix = "cntwp_";
+   
+?>
